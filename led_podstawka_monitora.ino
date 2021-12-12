1755ce25e7e2d6b18250fe97e68e74ec92676df8
Karol Augustyn <karol281298@gmai.com>
2021-12-12 19:32:48 +0100

    added pushbutton and pallete/color changes

--- led_podstawka_monitora.ino
+++ led_podstawka_monitora.ino
@@ -4,10 +4,10 @@
   rev: 0.1.2
 
   TODO:
-  -add ability to change palettes (button or so)
+  -add function to change pallete
+  -add function to change pattern
   -add abillity to change patterns (brightening and dimming <-> chain LED switching <-> linear changes)
   -BT module
-  -Add oalettes better than current one
   -what is blending??
 */
 #include <FastLED.h>
@@ -18,10 +18,21 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define DELAY_TIME 5
+#define CHAIN_TIME_MS 100
+#define LINEAR_TIME_MS 25
+#define BUTTON_PIN 2
+#define DIMMING 0
+#define CHAIN 1
+#define LINEAR 2
+#define DIMMING_COLOR_INCREMENT 16
+#define CHAIN_COLOR_INCREMENT 16
+#define LINEAR_COLOR_INCREMENT 1
 CRGB leds[NUM_LEDS];
 
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
+
+
 
 //----paletts
 
@@ -64,7 +75,7 @@ currentPalette = CRGBPalette16(yellow, pink, aqua,
                               yellow, pink, aqua, pink);
 }
 
-void ChangePallete(uint8_t index){
+void ChoosePalette(uint8_t index){
   if(index == 0){
     Setup1Pallete();
   }
@@ -83,47 +94,148 @@ void ChangePallete(uint8_t index){
 //-------------
 //---global
 uint8_t index = 0;
-uint8_t pallete_index = 1;
+uint8_t pallete_index = 0;
+volatile uint16_t counter = 0;
+uint8_t colorIndexIncrement = DIMMING_COLOR_INCREMENT;
+uint8_t pattern = DIMMING;
+
+long debouncing_time = 15; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros;
+volatile bool previusBtnState = 1;
 //-------------
+//----ISRs
+ISR(TIMER1_COMPA_vect){
+  //increment counter
+  counter++;
+}
+void BUTTON_CHANGE (){
+  if((long)(micros() - last_micros) >= debouncing_time * 1000) {
+  if(previusBtnState == 1){ 
+    counter = 0; 
+    Serial.println("falling");
+    previusBtnState = 0;
+  }else{ 
+    Serial.println("rising");
+    if(counter <= 1000){
+      ChangeCurrentPaletteIndex();
+    }
+    else{
+      ChangeColorTransitionPattern();
+    }
+    previusBtnState = 1;
+    counter = 0;
+  }
+  last_micros = micros();
+  }
+
+}
+//----
+
 void setup() {
-  // put your setup code here, to run once:
+  cli(); //stop interrupt
   delay( 3000 );
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), BUTTON_CHANGE, CHANGE);  
+  //----setting up the timer1 (interrupt every 1ms)
+  TCCR1A = 0; //clear register on startup (to run the timer OK every time)
+  TCCR1B = 0;
+  TCCR1B |= 0b011; // prescaler 64
+  TCCR1B |= (1<<WGM12); //turn on CTC mode
+  TCNT1 = 0; //initial timr1 value of 0
+  OCR1A = 249; // to have 1000hz interrupt (16 000 000/(64*1000)-1)
+  TIMSK1 |= (1<<OCIE1A); // turn on interrupt on compare Channel A
+  
+  //----
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
   Serial.begin(9600); // for debugging
 
-  ChangePallete(pallete_index);
-  //currentPalette = Tropical_Colors_gp;
-  currentBlending = NOBLEND;
+  ChoosePalette(pallete_index);
+  
+  currentBlending = LINEARBLEND;
 
+  sei(); //allow interrupts
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  FillLEDsFromPalleteColors(index); 
+  ChoosePalette(pallete_index);
+  switch (pattern){
+    case DIMMING:
+      FillLEDsFromPalleteColors(index); 
 
-  FastLED.show();
-  for(int i = 0; i <255;i++){
-    FastLED.setBrightness(i);
-    FastLED.show();
-    FastLED.delay(DELAY_TIME);
+      FastLED.show();
+      for(int i = 0; i <255;i++){
+        FastLED.setBrightness(i);
+        FastLED.show();
+        FastLED.delay(DELAY_TIME);
+      }
+      for(int i = 0; i <255;i++){
+        FastLED.setBrightness(255-i);
+        FastLED.show();
+        FastLED.delay(DELAY_TIME);
+      }
+
+      index = index + colorIndexIncrement;
+      break;
+
+    case CHAIN:
+      FastLED.setBrightness(BRIGHTNESS);
+      FillLEDsChainwise(index);
+      index = index + colorIndexIncrement;
+      break;
+
+    case LINEAR:
+      FastLED.setBrightness(BRIGHTNESS);
+      FillLEDsFromPalleteColors(index);
+      FastLED.show();
+      FastLED.delay(LINEAR_TIME_MS);
+      index = index + colorIndexIncrement;
+      break;
   }
-  for(int i = 0; i <255;i++){
-    FastLED.setBrightness(255-i);
-    FastLED.show();
-    FastLED.delay(DELAY_TIME);
-  }
-  
-  index = index + 16; 
 
 }
 
 
 void FillLEDsFromPalleteColors(uint8_t colorIndex) 
 {
-    Serial.println(colorIndex);
+    //Serial.println(colorIndex);
     for( int i = 0; i < NUM_LEDS; ++i) {
         leds[i] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
     }
 }
 
+void FillLEDsChainwise(uint8_t colorIndex){
+    for( int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+        FastLED.show();
+        FastLED.delay(CHAIN_TIME_MS);
+    }
+
+}
+
+void ChangeCurrentPaletteIndex(){
+  Serial.println("color change");
+  if(pallete_index == 2){
+    pallete_index = 0; 
+  }
+  else {
+    pallete_index = pallete_index + 1;
+  }
+}
+
+void ChangeColorTransitionPattern(){
+  Serial.println("pattern change");
+  if(pattern == DIMMING){
+    pattern = CHAIN;
+    colorIndexIncrement = CHAIN_COLOR_INCREMENT;
+  }
+  else if(pattern == CHAIN){
+    pattern = LINEAR;
+    colorIndexIncrement = LINEAR_COLOR_INCREMENT;
+  }
+  else {
+    pattern = DIMMING;
+    colorIndexIncrement = DIMMING_COLOR_INCREMENT;
+  }
+
+}
